344185c4f20206f705852e0cd00537e11fe3d032
SemyonDuB <dubskikhsemyon@gmail.com>
2025-04-12 23:54:20 +0500

    ~ fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,8 @@
 fish_add_path -ga ~/.local/bin/
 fish_add_path -ga ~/.cargo/bin/
 
-set -gx HELIX_RUNTIME ~/.local/share
+set -gx HELIX_RUNTIME ~/.local/share/helix/runtime/
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
