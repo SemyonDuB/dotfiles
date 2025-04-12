f788e1956bd59f2ca70e3db92c4c302bb21bc176
SemyonDuB <dubskikhsemyon@gmail.com>
2025-04-12 23:03:29 +0500

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 fish_add_path -ga ~/.local/bin/
 fish_add_path -ga ~/.cargo/bin/
 
+set -gx HELIX_RUNTIME ~/.local/share
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
